8ef9bb37cff146b38795c02061c1cac4b0e31d1d
Ricard Abril <ricardabril45@gmail.com>
2019-09-27 09:20:48 +0200

    ex6

--- S1/Entrega/Ex6.m
+++ S1/Entrega/Ex6.m
@@ -2,10 +2,16 @@ function [] = Ex6()
 x = 0.8:0.01:2
 n = rand(121)*0.1;
 n = n(1,:);
-
-y = (x * 0.21) + n;
+y = (x * 0.3) + n;
 y = y -mean(y);
 x = x - mean(x);
-scatter(x,y);
+z = [x;y];
+t = -atan(0.3);
+R = [cos(t) -sin(t);sin(t) cos(t)];
+pr = R*z;
+xr=pr(1,:);
+yr = pr(2,:);
+scatter(xr,yr);
+set(gca,'YAxisLocation','origin');
+set(gca,'XAxisLocation','origin');
 end
-
