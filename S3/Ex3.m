2c86d5733e0be3d3397a5e646ab312a332789730
Ricard Abril <ricardabril45@gmail.com>
2019-10-14 20:44:19 +0200

    EX2

--- S3/Ex3.m
+++ S3/Ex3.m
@@ -1,7 +1,4 @@
 function [outputArg1,outputArg2] = EX3(inputArg1,inputArg2)
-%EX3 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
 end
 
--- S3/Test_Ex2.m
+++ S3/Test_Ex2.m
@@ -1,5 +1,19 @@
-X = imread('img3.tif');
-%Y = rgb2gray(X);
-Y = imnoise(X,'salt & pepper',0.1);
-z = colfilt(Y,[5 5],'sliding',@Ex2);
-imshow(z);
+clear all; close all;
+I=imread('img2.jpeg');
+I=rgb2gray(I);
+noiselevel=0.2;
+figure
+subplot(131),imshow(I,[]);
+title 'Original Image'
+rand('seed', 0);
+J=I;
+FF=rand(size(J));
+J(FF<=(noiselevel/2))=0;
+J(FF>=(1-noiselevel/2) & FF<=1)=255;
+p_noise=psnr(double(I),double(J))
+subplot(1,3,2),imshow(J,[]);
+title 'Noisy Image'
+[M,F2]=Ex2(J);
+p_AWMF = psnr(double(I),double(M))
+subplot(133);imshow(M,[]);
+title 'Result'
