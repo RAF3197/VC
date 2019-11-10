dc0e3d148b90a6cba7c53b106e3280b09fd76943
Ricard Abril <ricardabril45@gmail.com>
2019-11-10 20:12:33 +0100

    Ex2 fet

---
+++ S6/bin_local.m
@@ -0,0 +1,17 @@
+function [P] = bin_local(win,k)
+rows = size(win,1);
+cols = size(win,2);
+pixels = floor((rows+1)/2);
+P = zeros(1,cols);
+for i = 1:cols
+    column = win(:,i);
+    Average = mean(column);
+    pixel = column(pixels);
+    if(pixel-k >= Average)
+        P(i) = 255;
+    else
+        P(i) = 0;
+    end
+end
+end
+
