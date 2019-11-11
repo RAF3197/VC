51a3045b4243b175780907d08e6430b179be6591
Ricard Abril <ricardabril45@gmail.com>
2019-11-11 18:13:13 +0100

    ex3

--- S5/ex3.m
+++ S5/ex3.m
@@ -7,26 +7,15 @@ B = I(:,:,3);
 RGB = [R(:),G(:),B(:)];
 RGB2 = kmeans(double(RGB),k);
 
-HSV = rgb2hsv(I);
-H = HSV(:,:,1);
-S = HSV(:,:,2);
-V = HSV(:,:,3);
-
-HSV = [V(:), S(:), sin(H(:)), cos(H(:))];
-HSV2 = kmeans(double(HSV),k);
-HSV3 = reshape(HSV2,tam(1), tam(2));
-
 RGB3 = reshape(RGB2,tam(1), tam(2));
 mapping = [ 1, 1, 0 
             0, 1, 1 
             0, 1, 0 
             0, 0, 1 
             1, 0, 0 ];
-
-%subplot(1,2,1);
-imshow(HSV3, mapping);
 %figure;
 %subplot(1,2,2);
+bwconncomp(RGB3)
 imshow(RGB3, mapping);
 end
 
