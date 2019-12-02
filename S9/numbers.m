43c413956e49818860eb1ce03fce207523d2f3e9
Xavi <xapaal@gmail.com>
2019-12-02 23:17:57 +0100

    fixing matricula

--- S9/numbers.m
+++ S9/numbers.m
@@ -5,6 +5,18 @@ function [] = numbers(matricula_posible)
     components = bwconncomp(BW);
     stats = regionprops(components,'all');
     
+    weight_1 = 0.3;
+    weight_2 = 0.05;
+    weight_3 = 0;
+    weight_4 = 0;
+    weight_5 = 0;
+    weight_6 = 0.35;
+    weight_7 = 0.2;
+    weight_8 = 0.09;
+    
+    Y = ['0';'1';'2';'3';'4';'5';'6';'7';'8';'9';'B';'C';'D';'F';'G';'H';'J';'K';'L';'M';'N';'P';'R';'S';'T';'V';'W';'X';'Y';'Z'];
+
+        
     %aqui hem de normalitzar 
     for i = 1:size(stats)
         statsValue1(i) = 1-stats(i).EulerNumber;
@@ -15,20 +27,24 @@ function [] = numbers(matricula_posible)
         statsValue6(i) = stats(i).Area/(stats(i).Perimeter*stats(i).Perimeter);
         statsValue7(i) = stats(i).Solidity;
         statsValue8(i) = stats(i).Eccentricity;
+        
+        matrix(i,1)=Y(i);
+        matrix2(i,1)=statsValue1(i);
+        matrix2(i,2)=statsValue2(i);
+        matrix2(i,3)=statsValue3(i);
+        matrix2(i,4)=statsValue4(i);
+        matrix2(i,5)=statsValue5(i);
+        matrix2(i,6)=statsValue6(i);
+        matrix2(i,7)=statsValue7(i);
+        matrix2(i,8)=statsValue8(i);
+
     end
     
-    statsValue1 = normalize(statsValue1, 'range');
-    statsValue2 = normalize(statsValue2, 'range');
-    statsValue3 = normalize(statsValue3, 'range');
-    statsValue4 = normalize(statsValue4, 'range');
-    statsValue5 = normalize(statsValue5, 'range');
-    statsValue6 = normalize(statsValue6, 'range');
-    statsValue7 = normalize(statsValue7, 'range');
-    statsValue8 = normalize(statsValue8, 'range');
     
     total = statsValue1 + statsValue2 + statsValue3 + statsValue4 + statsValue5 + statsValue6 + statsValue7 + statsValue8;
-
-    total = normalize(total, 'range');
+    total = statsValue1*weight_1+statsValue2*weight_2+statsValue3*weight_3+statsValue4*weight_4+statsValue5*weight_5+statsValue6*weight_6+statsValue7*weight_7+statsValue8*weight_8;
+    
+    %total = normalize(total, 'range');
     
     matricula_posible = matricula_posible < 70;
     components = bwconncomp(matricula_posible);
@@ -61,6 +77,17 @@ function [] = numbers(matricula_posible)
             statsValueRes6(count) = statsValueRes4(count) / (statsValueRes5(count) * statsValueRes5(count) );
             statsValueRes7(count) = statsR(i).Solidity;
             statsValueRes8(count) = statsR(i).Eccentricity;
+            
+            matrix3(count,1)=statsValueRes1(count);
+            matrix3(count,2)=statsValueRes2(count);
+            matrix3(count,3)=statsValueRes3(count);
+            matrix3(count,4)=statsValueRes4(count);
+            matrix3(count,5)=statsValueRes5(count);
+            matrix3(count,6)=statsValueRes6(count);
+            matrix3(count,7)=statsValueRes7(count);
+            matrix3(count,8)=statsValueRes8(count);
+        
+        
             count = count+1;
         end
     end
@@ -74,18 +101,16 @@ function [] = numbers(matricula_posible)
     for i = 1:size(count)
     end
     
-    statsValueRes1 = normalize(statsValueRes1, 'range');
-    statsValueRes2 = normalize(statsValueRes2, 'range');
-    statsValueRes3 = normalize(statsValueRes3, 'range');
-    statsValueRes4 = normalize(statsValueRes4, 'range');
-    statsValueRes5 = normalize(statsValueRes5, 'range');
-    statsValueRes6 = normalize(statsValueRes6, 'range');
-    statsValueRes7 = normalize(statsValueRes7, 'range');
-    statsValueRes8 = normalize(statsValueRes8, 'range');
     
     totalRes = statsValueRes1 + statsValueRes2 + statsValueRes3 + statsValueRes4 + statsValueRes5 + statsValueRes6 + statsValueRes7 + statsValueRes8;
-        
-    totalRes = normalize(totalRes, 'range');
+    totalRes = statsValueRes1*weight_1+statsValueRes2*weight_2+statsValueRes3*weight_3+statsValueRes4*weight_4+statsValueRes5*weight_5+statsValueRes6*weight_6+statsValueRes7*weight_7+statsValueRes8*weight_8;
+    
+    for i = 1:8
+        rest(i) = matrix2(26, i)-matrix3(7, i);
+    end
+    
+    
+    %totalRes = normalize(totalRes, 'range');
     
     total = total'
     totalRes = totalRes'
