bf8c6951a1c9eb4b0ca5d9e5c8bfa391ff042c97
Xavi <xapaal@gmail.com>
2019-12-03 17:55:40 +0100

    adding m

--- ShortProject/detector.m
+++ ShortProject/detector.m
@@ -5,6 +5,22 @@ function [] = detector(I)
     delete(h);
     rectangle('Position', userBox, 'Edgecolor', 'r');
     
+    redChannel = I(:,:,1); % Red channel
+    greenChannel = I(:,:,2); % Green channel
+    blueChannel = I(:,:,3); % Blue channel
+    
+    figure;
+    imshow(redChannel);
+    rectangle('Position', userBox, 'Edgecolor', 'r');
+    
+    figure;
+    imshow(greenChannel);
+    rectangle('Position', userBox, 'Edgecolor', 'r');
+    
+    figure;
+    imshow(blueChannel);
+    rectangle('Position', userBox, 'Edgecolor', 'r');
+    
     
     %M = ~h.createMask();
     %I(M) = 0;
