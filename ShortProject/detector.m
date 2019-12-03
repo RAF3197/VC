f4abab18160d89384e263e763256937b15cbd189
Xavi <xapaal@gmail.com>
2019-12-03 17:17:13 +0100

    adding code

--- ShortProject/detector.m
+++ ShortProject/detector.m
@@ -1,2 +1,12 @@
 function [] = detector(I)
+    imshow(I);
+    h = imrect; %draw something 
+    userBox = h.getPosition
+    delete(h);
+    rectangle('Position', userBox, 'Edgecolor', 'r');
+    
+    
+    %M = ~h.createMask();
+    %I(M) = 0;
+    %imshow(I);
 end
