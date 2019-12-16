25a47fab9aa2cc2efe9843033ed37ba0bb5265ea
Ricard Abril <ricardabril45@gmail.com>
2019-12-16 23:57:34 +0100

    probant de separar la selecio de la imatge

--- ShortProject/detector.m
+++ ShortProject/detector.m
@@ -21,6 +21,29 @@ function [] = detector(I)
     imshow(blueChannel);
     rectangle('Position', userBox, 'Edgecolor', 'r');
     
+    %Crop Imatge 
+    
+    redCrop = imcrop(redChannel,userBox);
+    greenCrop = imcrop(greenChannel,userBox);
+    blueCrop = imcrop(blueChannel,userBox);
+    
+    redBack = redChannel;
+    greenBack = greenChannel;
+    blueBack = blueChannel;
+    
+    for i=1:size(redChannel)
+        if (i>=userBox(1) || i<=userBox(3)-userBox(1))
+            for j=1:size(redChannel(i))
+                if (j>=userBox(2) || j<=userBox(4)-userBox(2))
+                    redBack(i ,j) = 0;
+                end
+            end
+        end
+    end
+    
+    
+    figure;
+    imshow(redBack);
     
     %M = ~h.createMask();
     %I(M) = 0;
