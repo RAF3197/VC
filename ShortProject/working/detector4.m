bc580c42e8db3cefdbc610f7d933aa5132c893c6
Ricard Abril <ricardabril45@gmail.com>
2020-01-17 01:11:56 +0100

    marker pending

--- ShortProject/working/detector4.m
+++ ShortProject/working/detector4.m
@@ -29,7 +29,7 @@ function [] = detector4(I)
     
     z=0;
     
-    IM2 = imread('Eagle2.png');
+    IM2 = imread('Snow2.png');
     IM2 = imresize(IM2, [4096, 4096]);
     cells2 = mat2tiles(IM2, [64, 64]);
     hog2 = extractHOGFeatures(cells2{1, 1});
@@ -158,23 +158,72 @@ backColorIt = 1;
     clasificador = fitcecoc([hog colorR colorG colorB ], labels);
 %     clasificador = fitcecoc(hog, labels);
     [label, score, cost] = predict(clasificador, [hog colorR colorG colorB ]);
-    x = 0;
+%     x = 0;
+%     for i=1:64
+%         for j=1:64
+%             x = x + 1; 
+%             for k = 1:64
+%                 for l = 1:64
+%                     if label(x) == "Foreground"
+%                     IM((i-1)*64+l,(j-1)*64+k,1) = 0;
+%                     IM((i-1)*64+l,(j-1)*64+k,2) = 0  ;
+%                     IM((i-1)*64+l,(j-1)*64+k,3) = 0;
+%                     end
+%                 end
+%             end
+%         end
+%     end
+%     figure;
+%     imshow(IM);
+    
+    %Binaritzem i eliminem soroll 
+    
+    IMGray = rgb2gray(IM);
+    fores = 0;
+        x = 0;
     for i=1:64
         for j=1:64
             x = x + 1; 
             for k = 1:64
                 for l = 1:64
                     if label(x) == "Foreground"
-                    IM((i-1)*64+l,(j-1)*64+k,1) = 256;
-%                     IM((i-1)*64+l,(j-1)*64+k,2) ;
-%                     IM((i-1)*64+l,(j-1)*64+k,3) = 0;
+                    IMGray((i-1)*64+l,(j-1)*64+k) = 0;
+                    fores = fores + 1;
+                    if labels(x) == "Background"
+                        IMGray((i-1)*64+l,(j-1)*64+k) = 256;
                     end
+                    else
+                    IMGray((i-1)*64+l,(j-1)*64+k) = 256;
+                    end
+                    
                 end
             end
         end
     end
+
+%     BW = imbinarize(IMGray,'global');
+figure;
+    imshow(IMGray);
+%     IMGray = imcrop(IMGray,userBox);
+    
+    BW = bwareaopen(IMGray,64 * 64 * 64,18);
     figure;
-    imshow(IM);
+    imshow(BW);
+    se = strel('disk',8);
+    closeBW = imclose(BW,se); figure, imshow(closeBW)
+    %a = bwconncomp(closeBW,18);
+    a = bwareafilt(closeBW,1);
+    
+    figure;
+    imshow(a);
+    
+    BW2 = bwareafilt(closeBW);
+    figure;
+    imshow(BW2)
+    
+    %Reseguim amb un Marker la CC + gran que trobem en la imatge.
+    
+    
     [label2, score, cost] = predict(clasificador,[hog2 color2R color2G color2B]);
     x = 0;
     for i=1:64
